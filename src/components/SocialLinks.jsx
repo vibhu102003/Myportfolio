94b105f30736fd03148cebd883b4e53123f92efe
vibhu102003 <vibhugupta498@gmail.com>
2023-10-25 06:22:48 +0530

    second commit

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -60,7 +60,7 @@ const links= [
         <ul>
             {links.map(({id,child,href,style,download})=>(
 
-            <li  key={id}className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'+" "+style}>
+            <li  key={id}className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500".concat(" "+style)}>
                <a href={href} className='flex justify-between items-center w-full text-white' download={download} target="_blank"
                rel="noreferrer"
                >
